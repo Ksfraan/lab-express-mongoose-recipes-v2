75546055bfa5eafb6d97a4993cf4f412e54652ec
“Carina <ksfraan@hotmail.com>
2024-01-17 16:25:55 +0000

    iterations 4, 5, 6, and 7 done

--- app.js
+++ app.js
@@ -15,10 +15,7 @@ app.use(express.urlencoded({ extended: false }));
 const MONGODB_URI = 'mongodb://localhost:27017/express-mongoose-recipes-dev';
 
 mongoose
-    .connect('mongodb://localhost:27017/express-mongoose-recipes-dev', {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
+    .connect(MONGODB_URI)
     .then((x) =>
         console.log(
             `Connected to Mongo! Database name: "${x.connections[0].name}"`
@@ -33,7 +30,7 @@ app.get('/', (req, res) => {
 });
 
 //  Iteration 3 - Create a Recipe route
-app.post('/api/recipes', async (request, response) => {
+app.post('/recipes', async (request, response) => {
     console.log(request.body);
     const payload = request.body;
     try {
@@ -47,16 +44,52 @@ app.post('/api/recipes', async (request, response) => {
     }
 });
 //  Iteration 4 - Get All Recipes
-//  GET  /recipes route
+app.get('/recipes', async (request, response) => {
+    const recipes = await Recipe.find();
+    response.json(recipes);
+});
 
 //  Iteration 5 - Get a Single Recipe
-//  GET  /recipes/:id route
+app.get('/recipes/:recipeId', async (request, response) => {
+    const { recipeId } = request.params;
+
+    const oneRecipe = await Recipe.findById(recipeId);
+    response.json(oneRecipe);
+});
 
 //  Iteration 6 - Update a Single Recipe
-//  PUT  /recipes/:id route
+app.put('/recipes/:recipeId', async (request, response) => {
+    console.log(request.body);
+    const payload = request.body;
+    try {
+        const updatedRecipe = await Recipe.findByIdAndUpdate(
+            request.params.recipeId,
+            payload,
+            { new: true }
+        );
+        response.status(202).json(updatedRecipe);
+    } catch (error) {
+        console.log(error);
+        response.status(500).json({ message: 'Something happened' });
+    }
+});
 
 //  Iteration 7 - Delete a Single Recipe
-//  DELETE  /recipes/:id route
+app.delete('/recipes/:recipeId', async (request, response) => {
+    const { recipeId } = request.params;
+    try {
+        const recipeToDelete = await Recipe.findById(recipeId);
+        await Recipe.findByIdAndDelete(recipeId);
+        response
+            .status(202)
+            .json({
+                message: `${recipeToDelete.title} was removed from the db`,
+            });
+    } catch (error) {
+        console.log(error);
+        response.status(500).json({ message: 'Something bad happened' });
+    }
+});
 
 // Start the server
 app.listen(5005, () => console.log('My first app listening on port 5005!'));
